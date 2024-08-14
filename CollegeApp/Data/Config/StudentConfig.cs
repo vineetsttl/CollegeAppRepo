15c51dfb794d0f983321047e420a8f838069658d
Vineet Shah <shah.vineet@silvertouch.com>
2024-08-14 17:42:01 +0530

    Initial Commit

---
+++ CollegeApp/Data/Config/StudentConfig.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CollegeApp.Data.Config
+{
+    public class StudentConfig : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Students");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(n => n.StudentName).IsRequired();
+            builder.Property(n => n.StudentName).HasMaxLength(250);
+            builder.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            builder.Property(n => n.Email).IsRequired().HasMaxLength(250);
+
+            builder.HasData(new List<Student>()
+            {
+                new Student {
+                    Id = 1,
+                    StudentName = "Vineet",
+                    Address="India",
+                    Email="vineet@gmail.com",
+                    DOB = new DateTime(2022,12,12)
+                },
+                new Student {
+                    Id = 2,
+                    StudentName = "SilverTouch",
+                    Address="India",
+                    Email="silvertouch@gmail.com",
+                    DOB = new DateTime(2022,06,12)
+                }
+            });
+
+            builder.HasOne(n => n.Department)
+                .WithMany(n => n.Students)
+                .HasForeignKey(n => n.DepartmentId)
+                .HasConstraintName("FK_Students_Department");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Silvertouch
+# This is practice repository used in the Asp.Net core web api tutorial version 8.0, 7.0 
+# Please visit & subscribe to https://www.youtube.com/@silvertouch to watch all latest technical videos  
